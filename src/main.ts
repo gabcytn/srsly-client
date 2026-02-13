26330ddcc65ec92a9ceaee77ce3eaa8cd4b458f4
gabcytn <cayetanogabriel03@gmail.com>
2026-02-13 17:41:43 +0800

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(router)
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura
+  }
+})
+
+app.mount('#app')
