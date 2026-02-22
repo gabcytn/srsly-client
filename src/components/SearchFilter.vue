efc7a1eb68227dcfde4a64b28f71fad0c8a4d07d
gabcytn <cayetanogabriel03@gmail.com>
2026-02-22 17:40:10 +0800

    feat(dashboard): sample headers

---
+++ src/components/SearchFilter.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const props = defineProps<{
+  label: string;
+  selected: boolean;
+}>();
+defineEmits(["click"]);
+console.log(`filter: ${props.label}`);
+console.log(`selected: ${props.selected}`);
+</script>
+<template>
+  <div
+    class="rounded border sm:px-5 text-center py-1 hover:border-primary cursor-pointer sm:grow-0 grow"
+    :class="{
+      'border-primary bg-primary-100 dark:bg-primary-950': selected,
+      'border-surface bg-mycard': !selected,
+    }"
+    @click="$emit('click', label)"
+  >
+    <span class="text-xs">{{ label }}</span>
+  </div>
+</template>
--- src/pages/DashboardView.vue
+++ src/pages/DashboardView.vue
@@ -1,6 +1,40 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed, ref } from "vue";
+
+const options: Intl.DateTimeFormatOptions = {
+  weekday: "long",
+  month: "long",
+  day: "numeric",
+};
+const formattedDate = new Date().toLocaleDateString("en-US", options);
+const completed = ref(3);
+const reviews = ref(7);
+
+const percentage = computed(() => (completed.value / reviews.value) * 100);
+</script>
 
 <template>
-  <SidebarNav />
-  <SidebarDrawer />
+  <div class="md:flex">
+    <SidebarNav />
+    <main class="max-w-275 w-full mx-auto p-5">
+      <h1 class="font-bold text-xl">Dashboard</h1>
+      <p class="text-light text-sm">{{ formattedDate }}</p>
+
+      <div class="bg-mycard mt-5 p-3 rounded border border-surface">
+        <span class="text-light text-xs inline-block mb-2">Today's progress</span>
+        <div class="flex justify-between gap-3 items-center">
+          <ProgressBar class="grow" :value="percentage">{{}}</ProgressBar>
+          <p>
+            <span class="font-bold me-2 text-xl">{{ completed }}</span
+            ><span class="text-xs text-light">/ {{ reviews }} reviewed</span>
+          </p>
+        </div>
+      </div>
+
+      <div class="mt-5">
+        <h1 class="font-bold text-xl">Review Today</h1>
+        <SearchBar />
+      </div>
+    </main>
+  </div>
 </template>
