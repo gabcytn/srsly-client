d10b1ce6cbc393ed7904307bea3130a11044d1a4
gabcytn <cayetanogabriel03@gmail.com>
2026-03-15 14:41:45 +0800

    fix: tag color

--- src/components/problem-show/ai/SummaryCritique.vue
+++ src/components/problem-show/ai/SummaryCritique.vue
@@ -3,14 +3,25 @@ import type { Summary } from "@/shared/types";
 import capitalizeWord from "@/utils/capitalize-word";
 import { Tag } from "primevue";
 
-defineProps<{
+const props = defineProps<{
   summary: Summary;
 }>();
+
+function getTagSeverity() {
+  switch (props.summary.verdict) {
+    case "Accepted":
+      return "success";
+    case "Rejected":
+      return "danger";
+    case "ConditionallyAccepted":
+      return "warn";
+  }
+}
 </script>
 
 <template>
   <div class="gap-2 items-center">
-    <Tag severity="success" :value="summary.verdict" icon="pi pi-circle-fill" />
+    <Tag :severity="getTagSeverity()" :value="summary.verdict" icon="pi pi-circle-fill" />
     <div class="text-sm">
       Rating: <span class="font-bold">{{ summary.overallRating }}</span>
     </div>
