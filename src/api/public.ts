c04e062a9de705104cff41c55f0017f4d955963f
gabcytn <cayetanogabriel03@gmail.com>
2026-05-14 17:03:33 +0800

    feat: toast message for rate limited routes

--- src/api/public.ts
+++ src/api/public.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { ToastEventBus, type ToastMessageOptions } from "primevue";
 
 const SERVER_URL = import.meta.env.VITE_SERVER_URL;
 
@@ -8,6 +9,19 @@ const api = axios.create({
   withCredentials: true,
 });
 
-api.interceptors.response.use((response) => response.data);
+api.interceptors.response.use(
+  (response) => response.data,
+  async (error) => {
+    if (error.response.status === 429) {
+      const toast = { add: (message: ToastMessageOptions) => ToastEventBus.emit("add", message) };
+      toast.add({
+        severity: "error",
+        summary: "Too many requests",
+        detail: "Try again later.",
+        life: 3000,
+      });
+    }
+  },
+);
 
 export default api;
